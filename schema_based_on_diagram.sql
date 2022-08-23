f65a8667f51790b99f8277c8597c429122a98edd
Dozie Udeagha <dozie.udeagha@gmail.com>
2022-08-23 14:52:05 +0100

    Update schema_based_on_diagram.sql
    Update table creation

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -12,3 +12,46 @@ CREATE TABLE "invoices"(
     "payed_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
     "medical_history_id" INTEGER NOT NULL
 );
+ALTER TABLE
+    "invoices" ADD PRIMARY KEY("id");
+CREATE TABLE "medical_histories"(
+    "id" INTEGER NOT NULL,
+    "admitted_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+    "patient_id" INTEGER NOT NULL,
+    "status" VARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    "medical_histories" ADD PRIMARY KEY("id");
+CREATE TABLE "treatment"(
+    "id" INTEGER NOT NULL,
+    "type" VARCHAR(255) NOT NULL,
+    "name" VARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    "treatment" ADD PRIMARY KEY("id");
+CREATE TABLE "invoice_items"(
+    "id" INTEGER NOT NULL,
+    "unit_price" DECIMAL(8, 2) NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "total_price" DECIMAL(8, 2) NOT NULL,
+    "invoice_id" INTEGER NOT NULL,
+    "treatement_id" INTEGER NOT NULL
+);
+ALTER TABLE
+    "invoice_items" ADD PRIMARY KEY("id");
+CREATE TABLE "treatment_histories"(
+    "medical_histories_id" INTEGER NOT NULL,
+    "treatment_id" INTEGER NOT NULL
+);
+ALTER TABLE
+    "treatment_histories" ADD PRIMARY KEY("medical_histories_id");
+ALTER TABLE
+    "treatment_histories" ADD PRIMARY KEY("treatment_id");
+ALTER TABLE
+    "invoice_items" ADD CONSTRAINT "invoice_items_invoice_id_foreign" FOREIGN KEY("invoice_id") REFERENCES "invoices"("id");
+ALTER TABLE
+    "medical_histories" ADD CONSTRAINT "medical_histories_patient_id_foreign" FOREIGN KEY("patient_id") REFERENCES "patients"("id");
+ALTER TABLE
+    "invoices" ADD CONSTRAINT "invoices_medical_history_id_foreign" FOREIGN KEY("medical_history_id") REFERENCES "medical_histories"("id");
+ALTER TABLE
+    "invoice_items" ADD CONSTRAINT "invoice_items_treatement_id_foreign" FOREIGN KEY("treatement_id") REFERENCES "treatment"("id");
